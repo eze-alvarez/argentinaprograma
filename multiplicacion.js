bc80a4e415f5041e24d91e2416e2a6da9e77206c
Ezequiel Alvarez <ezze.alvarez.2013@gmail.com>
2023-06-01 21:37:16 -0300

    first commit

---
+++ multiplicacion.js
@@ -0,0 +1,14 @@
+let a;
+let b;
+let c;
+
+function multiplicar(a,b,c){
+    let multiplicacion = a * b * c;
+    console.log(multiplicacion);
+    return multiplicacion;
+}
+
+
+multiplicar(2,17,6)
+
+// ¿Qué valor devuelve su programa si los números son 2 *17 * 6?    --->    devuelve 204
---
+++ resta.js
@@ -0,0 +1,17 @@
+let minuendo;
+let sustraendo;
+
+
+function resta(minuendo,sustraendo){
+    let resta = minuendo - sustraendo;
+    console.log(resta)
+    return resta
+}
+
+resta(10,2);
+
+// ¿Qué valor devuelve su programa si el minuendo es 10 y el sustraendo 2?  ---> devuelve 8
+
+
+
+
